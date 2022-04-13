f69431005da01021c17c4f78d96804ea72eea74b
suarezmariano <marianosuarez@hotmail.com>
2022-04-12 23:08:15 -0300

    correcion de detail y list y inst de method override

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(methodOverride('_method'));
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
